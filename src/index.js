04ee7969c2063bc4a7bf821988dc6cdc926d8137
keerthi-smc2 <134478591+keerthi-smc2@users.noreply.github.com>
2024-11-05 14:08:17 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Firstpage from './pages/Firstpage.jsx';
+// import "bootstrap/dist/css/bootstrap.min.css";
+import Hooks from './pages/Hooks.jsx';
+// import store from './redux/store.js';
+// import { Provider } from 'react-redux';
+import Qty from '../src/components/main/productTile/Qty.jsx';
+// import { BrowserRouter, Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home.js';
+import Nopage from './components/Nopage.js';
+import BoysFashion from './components/BoysFashion.jsx';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    {/* <BrowserRouter> */}
+     {/* <Routes> 
+       <Route index element={<Home />} />
+       <Route path='/home' element={<Home />} />
+       <Route path='/boysfashion' element={<BoysFashion />} />  */}
+      
+      {/* <Route path='*' element={<Nopage />} />  */}
+    <Firstpage />
+    {/* <Qty /> */}
+   {/* <Hooks /> */}
+   {/* </Routes> */}
+   {/* </BrowserRouter> */}
+  </>
 );
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <Hooks />
+//   </Provider>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,10 @@
-import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './rootReducer';
-import Logger from 'redux-logger';
-import { thunk } from 'redux-thunk'; // Change this line
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { createStore } from 'redux';
+import rootReducer from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer, applyMiddleware(Logger, thunk));
-
-// const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(Logger, thunk)));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools()
+);
 
 export default store;
